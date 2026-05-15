8e490247cb7277a0f29d343b9b5a703557223674
RaghunadhSahitDruvam <raghunadhwinwin@gmail.com>
2026-05-15 19:04:29 +0530

    Updated Daily analysis for 15-05-26 - Volumetric-Ulgo

---
+++ generated-output/frontend/data/volumetric/daily/15-05-26.ts
@@ -0,0 +1,117 @@
+// @ts-nocheck 
+    // Generated on: 2026-05-15T13:34:26.366Z
+    // Indicator: Volumetric-Ulgo
+    // Timeframe: Daily
+    // Total Stocks: 0
+    
+    const stockData = [
+  {
+    "scrip_cd": 532654,
+    "scripname": "MCLEODRUSS",
+    "LONG_NAME": "Mcleod Russel India Ltd",
+    "scrip_grp": "T",
+    "openrate": 61.5,
+    "highrate": 62.74,
+    "lowrate": 60.17,
+    "ltradert": 62.53,
+    "prevdayclose": 59.76,
+    "change_val": 2.77,
+    "change_percent": 4.64,
+    "index_code": "",
+    "trd_val": 139.77,
+    "trd_vol": 223922,
+    "nooftrd": 442,
+    "trend": "+",
+    "dt_tm": "2026-05-15T16:00:00",
+    "Ishighflag": 0,
+    "IsLowflag": 0,
+    "URL": "https://www.bseindia.com/stock-share-price/mcleod-russel-india-ltd/mcleodruss/532654/",
+    "NSUrl": "https://www.bseindia.com/stock-share-price/mcleod-russel-india-ltd/mcleodruss/532654/",
+    "CA": "2018-08-01T00:00:00",
+    "isBreakout": true,
+    "value": 59.09,
+    "comp_name": {
+      "name_g1_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>64</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>50</td><td>86</td></tr><tr><td>&nbsp;</td><td>47</td><td>75</td><td>111</td></tr><tr><td>25</td><td>22</td><td>28</td><td>36</td></tr><tr><td>MCLEOD</td><td>RUSSEL</td><td>INDIA</td><td>LIMITED</td></tr>",
+      "name_g2_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>34</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>34</td><td>56</td></tr><tr><td>&nbsp;</td><td>48</td><td>60</td><td>82</td></tr><tr><td>26</td><td>22</td><td>12</td><td>22</td></tr><tr><td>MCLEOD</td><td>RUSSEL</td><td>INDIA</td><td>LIMITED</td></tr>",
+      "name_g3_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>109</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>131</td><td>203</td></tr><tr><td>&nbsp;</td><td>146</td><td>183</td><td>255</td></tr><tr><td>52</td><td>94</td><td>37</td><td>72</td></tr><tr><td>MCLEOD</td><td>RUSSEL</td><td>INDIA</td><td>LIMITED</td></tr>",
+      "tot_letters": 24,
+      "g1tot": 111,
+      "g2tot": 82,
+      "g3tot": 255,
+      "g1vtot": 61,
+      "g2vtot": 33,
+      "g3vtot": 88,
+      "g1nettot": 50,
+      "g2nettot": 49,
+      "g3nettot": 167,
+      "dob_tot": 0
+    },
+    "current_market_price": 62.53,
+    "trendline_strength": 0,
+    "pivot_point_strength": 0,
+    "ema_strength": 0,
+    "rs_strength": 0,
+    "ai_analysis": "AI Analysis Error: 401 \"Invalid API key\""
+  },
+  {
+    "scrip_cd": 526873,
+    "scripname": "RAJSEC",
+    "LONG_NAME": "Rajasthan Securities Ltd",
+    "scrip_grp": "X",
+    "openrate": 45.55,
+    "highrate": 45.75,
+    "lowrate": 45.55,
+    "ltradert": 45.75,
+    "prevdayclose": 45.55,
+    "change_val": 0.2,
+    "change_percent": 0.44,
+    "index_code": "",
+    "trd_val": 1.98,
+    "trd_vol": 4324,
+    "nooftrd": 11,
+    "trend": "+",
+    "dt_tm": "2026-05-15T16:00:00",
+    "Ishighflag": 0,
+    "IsLowflag": 0,
+    "URL": "https://www.bseindia.com/stock-share-price/rajasthan-securities-ltd/rajsec/526873/",
+    "NSUrl": "https://www.bseindia.com/stock-share-price/rajasthan-securities-ltd/rajsec/526873/",
+    "CA": "2020-01-16T00:00:00",
+    "isBreakout": true,
+    "value": 45.68,
+    "comp_name": {
+      "name_g1_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>83</td></tr><tr><td>&nbsp;</td><td>76</td><td>112</td></tr><tr><td>29</td><td>47</td><td>36</td></tr><tr><td>RAJASTHAN</td><td>SECURITIES</td><td>LIMITED</td></tr>",
+      "name_g2_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>55</td></tr><tr><td>&nbsp;</td><td>56</td><td>78</td></tr><tr><td>23</td><td>33</td><td>22</td></tr><tr><td>RAJASTHAN</td><td>SECURITIES</td><td>LIMITED</td></tr>",
+      "name_g3_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>200</td></tr><tr><td>&nbsp;</td><td>220</td><td>292</td></tr><tr><td>92</td><td>128</td><td>72</td></tr><tr><td>RAJASTHAN</td><td>SECURITIES</td><td>LIMITED</td></tr>",
+      "tot_letters": 26,
+      "g1tot": 112,
+      "g2tot": 78,
+      "g3tot": 292,
+      "g1vtot": 57,
+      "g2vtot": 28,
+      "g3vtot": 75,
+      "g1nettot": 55,
+      "g2nettot": 50,
+      "g3nettot": 217,
+      "dob_tot": 0
+    },
+    "current_market_price": 45.75,
+    "trendline_strength": 0,
+    "pivot_point_strength": 0,
+    "ema_strength": 0,
+    "rs_strength": 0,
+    "ai_analysis": "AI Analysis Error: 401 \"Invalid API key\""
+  }
+];
+    
+    const metadata = {
+  "indicatorName": "Volumetric-Ulgo",
+  "timeframe": "1d",
+  "timeframeDisplayName": "Daily",
+  "timestamp": "2026-05-15T13:34:26.366Z",
+  "totalStocks": 0,
+  "generatedDate": "15-05-26",
+  "config": {}
+};
+    
+    export { stockData as data, metadata };
+    export default stockData;
