3519526e64aa6cae3b0e8151eba6a624baa90050
RaghunadhSahitDruvam <raghunadhwinwin@gmail.com>
2026-05-22 18:36:48 +0530

    Updated Daily analysis for 22-05-26 - Volumetric-Ulgo

---
+++ generated-output/frontend/data/volumetric/daily/22-05-26.ts
@@ -0,0 +1,69 @@
+// @ts-nocheck 
+    // Generated on: 2026-05-22T13:06:42.873Z
+    // Indicator: Volumetric-Ulgo
+    // Timeframe: Daily
+    // Total Stocks: 0
+    
+    const stockData = [
+  {
+    "scrip_cd": 539900,
+    "scripname": "DIDL",
+    "LONG_NAME": "Dalmia Industrial Development Ltd",
+    "scrip_grp": "X",
+    "openrate": 12.37,
+    "highrate": 12.37,
+    "lowrate": 12.29,
+    "ltradert": 12.29,
+    "prevdayclose": 11.79,
+    "change_val": 0.5,
+    "change_percent": 4.24,
+    "index_code": null,
+    "trd_val": 0.53,
+    "trd_vol": 4344,
+    "nooftrd": 18,
+    "trend": "+",
+    "dt_tm": "2026-05-22T16:00:00",
+    "Ishighflag": 0,
+    "IsLowflag": 0,
+    "URL": "https://www.bseindia.com/stock-share-price/dalmia-industrial-development-ltd/didl/539900/",
+    "NSUrl": "https://www.bseindia.com/stock-share-price/dalmia-industrial-development-ltd/didl/539900/",
+    "CA": null,
+    "isBreakout": true,
+    "value": 11.15,
+    "comp_name": {
+      "name_g1_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>86</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>96</td><td>132</td></tr><tr><td>&nbsp;</td><td>68</td><td>118</td><td>154</td></tr><tr><td>22</td><td>46</td><td>50</td><td>36</td></tr><tr><td>DALMIA</td><td>INDUSTRIAL</td><td>DEVELOPMENT</td><td>LIMITED</td></tr>",
+      "name_g2_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>78</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>86</td><td>108</td></tr><tr><td>&nbsp;</td><td>44</td><td>100</td><td>122</td></tr><tr><td>14</td><td>30</td><td>56</td><td>22</td></tr><tr><td>DALMIA</td><td>INDUSTRIAL</td><td>DEVELOPMENT</td><td>LIMITED</td></tr>",
+      "name_g3_block": "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>203</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>258</td><td>330</td></tr><tr><td>&nbsp;</td><td>167</td><td>298</td><td>370</td></tr><tr><td>40</td><td>127</td><td>131</td><td>72</td></tr><tr><td>DALMIA</td><td>INDUSTRIAL</td><td>DEVELOPMENT</td><td>LIMITED</td></tr>",
+      "tot_letters": 34,
+      "g1tot": 154,
+      "g2tot": 122,
+      "g3tot": 370,
+      "g1vtot": 77,
+      "g2vtot": 41,
+      "g3vtot": 104,
+      "g1nettot": 77,
+      "g2nettot": 81,
+      "g3nettot": 266,
+      "dob_tot": 0
+    },
+    "current_market_price": 12.29,
+    "trendline_strength": 2.19,
+    "pivot_point_strength": 12.33,
+    "ema_strength": 0,
+    "rs_strength": 0,
+    "ai_analysis": "AI Analysis Error: 401 \"Invalid API key\""
+  }
+];
+    
+    const metadata = {
+  "indicatorName": "Volumetric-Ulgo",
+  "timeframe": "1d",
+  "timeframeDisplayName": "Daily",
+  "timestamp": "2026-05-22T13:06:42.873Z",
+  "totalStocks": 0,
+  "generatedDate": "22-05-26",
+  "config": {}
+};
+    
+    export { stockData as data, metadata };
+    export default stockData;
